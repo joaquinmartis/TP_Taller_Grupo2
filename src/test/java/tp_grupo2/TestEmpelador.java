f41d7826d249a14c3bddb111bccf08e2f351721c
Joaquin <joaquinmartinezfranco@gmail.com>
2023-10-18 14:55:35 -0300

    puse un txt con las clases hechas

---
+++ src/test/java/tp_grupo2/TestEmpelador.java
@@ -0,0 +1,40 @@
+package tp_grupo2;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import modeloDatos.EmpleadoPretenso;
+import modeloDatos.Empleador;
+
+public class TestEmpelador {
+	Empleador empleador;
+	EmpleadoPretenso empleadopretenso;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
