8740f0fe4e6dc156fd10594e56d69479a8489a31
Joaquin <joaquinmartinezfranco@gmail.com>
2023-10-25 13:39:59 -0300

    arregle assert

--- src/test/java/tp_grupo2/TestTicketConParametros.java
+++ src/test/java/tp_grupo2/TestTicketConParametros.java
@@ -8,7 +8,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import junit.framework.Assert;
+import org.junit.Assert;
 import modeloDatos.Ticket;
 
 public class TestTicketConParametros {
